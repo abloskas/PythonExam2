a72341076fe2dcc41f710b528fcec1537c9243a1
Ashley <a.bloskas@yahoo.com>
2018-03-28 09:06:26 -0700

    made change to index

--- exam_main/settings.py
+++ exam_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o_k0h4bj*=(jkcf1q1@5k@(a%a*t%_13-*wu-)vbhs_*stw*i7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.57.31.217']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
